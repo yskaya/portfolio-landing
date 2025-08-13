833851cb728bf478124e2da6921b46f4e348aff4
July Skaya <yulia.kanapatskaya@gmail.com>
2025-08-13 02:12:08 -0700

    fix style regression

--- src/app/motions/MotionFadeIn.tsx
+++ src/app/motions/MotionFadeIn.tsx
@@ -26,7 +26,7 @@ export function MotionFadeIn({ as = "div", className, children, delay = 0, once 
       whileInView="animate"
       viewport={{ once }}
       transition={{ ...slowSpring, duration: 0.6, delay }}
-      style={{ willChange: "opacity, transform" }}
+      style={{ willChange: "opacity, transform", ...style }}
     >
       {children}
     </Component>
