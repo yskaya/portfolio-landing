3492bc879555ade7cffda291d1e20ef376c6ca5b
July Skaya <yulia.kanapatskaya@gmail.com>
2025-08-13 21:15:36 -0700

    deletes extra files

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,5 +1,5 @@
 import { m } from 'motion/react';
-import { MotionFadeIn } from "../motions/MotionFadeIn";
+import { MotionFadeIn } from "../graphs/MotionFadeIn";
 import { useMousePosition } from '../hooks/useMousePosition';
 import { useScrollPosition } from '../hooks/useScrollPosition';
 import { Code2, Globe, Award, Users } from 'lucide-react';
